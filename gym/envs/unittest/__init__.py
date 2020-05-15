adef0ed6ef5a970f14b3a8df424189077689f03e
Cesare-Caputo <62548290+Cesare-Caputo@users.noreply.github.com>
2020-05-15 15:08:15 +0100

    Update __init__.py

--- gym/envs/unittest/__init__.py
+++ gym/envs/unittest/__init__.py
@@ -2,4 +2,4 @@ from gym.envs.unittest.cube_crash import CubeCrash
 from gym.envs.unittest.cube_crash import CubeCrashSparse
 from gym.envs.unittest.cube_crash import CubeCrashScreenBecomesBlack
 from gym.envs.unittest.memorize_digits import MemorizeDigits
-
+from gym.envs.unittest.Garage_RL_environment import Garage_Env
